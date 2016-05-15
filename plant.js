328ca7a3000ac9d50821a0debfb1f49c11e49db8
Justin Wayne <wayne.jch@gmail.com>
2016-05-15 10:01:54 -0500

    cleaned up plant.js
    added server uri - plant.js now functional.

--- plant.js
+++ plant.js
@@ -1,34 +1,35 @@
 var request = require('request-promise')
 
 
-//grab a bunch of stuff from the raspberry pi
-	//code code code. Stand in. see Blink.js example.
-
-
-//take info from the raspberry pi and POST it to the server
-var dummyData = {
-	'TJ' : 'BOY',
-	'Natalie' : 'GIRL',
-	'Bob' : 'PLANT'
-};
-
-var reqObject = {
-	method: 'POST',
-	uri: /* Stand in url **/,
-	body: dummyData,
-	json: true
-};
+//STAND IN: grab moisture data from GPIO sesnor
+	//code code code. See Blink.js example.
 
 
 exports.sendPlantInfo = function(plantInfo){
+//Sent plant info via HTTP POST to central server	
+	var reqObject = {
+		method: 'POST',
+		uri: 'https://safe-headland-15906.herokuapp.com/',
+		body: {},
+		json: true
+	};
+	
+	reqObject.data.plantInfo = plantInfo;
+	
 	request(reqObject)
 	.then(function(resp){
-		console.log('WE GOT DIS: ', resp);
+		console.log('WE GOT DIS (success): ', resp);
 	})
 	.catch(function(err){
 		console.error('WE GOT DAT (error): ', err);
 	})
 };
 
-//testing sending info:
-exports.sendPlantInfo();
+//test exports.sendPlantInfo
+var dummyData = {
+	'TJ' : 'BOY',
+	'Natalie' : 'GIRL',
+	'Bob' : 'PLANT'
+};
+
+exports.sendPlantInfo(dummyData);
